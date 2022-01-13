48948693fbc6664336434e321d8785479c450550
Pierrick Delrieu <58939886+pierrickdelrieu@users.noreply.github.com>
2022-01-13 16:05:47 +0100

    Add files via upload

---
+++ static/js/home_page.js
@@ -0,0 +1,5 @@
+balise_a = document.querySelector("#navigation").querySelectorAll("a")
+for (const element of balise_a){
+    element.classList.remove("actual_page");
+}
+balise_a[0].classList.add("actual_page");
